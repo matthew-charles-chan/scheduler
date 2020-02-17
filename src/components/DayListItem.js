1fab28e44cace2fb7bf0af0991aa9a6f24888c08
Matthew Charles Chan <matthew.charles.chan@gmail.com>
2020-02-17 11:26:37 -0800

    daylist items i

---
+++ src/components/DayListItem.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "components/DayListItem.scss";
+
+const classnames = require("classnames");
+
+export default function DayListItem(props) {
+  const dayClass = classnames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  })
+
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name) }>
+      <h2 className={dayClass}>{props.name}</h2>
+      <h3 className={dayClass}>{props.spots}</h3>
+    </li>
+  );
+}
