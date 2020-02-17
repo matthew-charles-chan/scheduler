0e4324e7d7c1cf2a64665cbf0071805cdffce2c3
Matthew Charles Chan <matthew.charles.chan@gmail.com>
2020-02-17 11:57:43 -0800

    format spots to "spots remainging" string

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,11 +8,20 @@ export default function DayListItem(props) {
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0
   })
-
+  const formatSpots = function(spots) {
+    switch(spots) {
+      case 0:
+        return 'no spots remaining';
+      case 1: 
+        return `1 spot remaining`;
+      default: 
+        return `${spots} spots remaining`;
+    }
+  }
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name) }>
       <h2 className={dayClass}>{props.name}</h2>
-      <h3 className={dayClass}>{props.spots}</h3>
+  <h3 className={dayClass}>{formatSpots(props.spots)}</h3>
     </li>
   );
 }
