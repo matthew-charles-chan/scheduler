fa7980c7251b3a8226d002eea3aa0f4814ce0a8d
Matthew Charles Chan <matthew.charles.chan@gmail.com>
2020-02-17 15:31:24 -0800

    create interviewers list, refactor daylist to make <ul>

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,16 +2,19 @@ import React from 'react';
 import DayListItem from "components/DayListItem";
 
 export default function DayList(props) {
-  const days = props.days.map(day => {
     return (
-      <DayListItem
-        key={day.id}
-        name={day.name}
-        spots={day.spots}
-        selected={day.name === props.day}
-        setDay={props.setDay}
-      />
-    );
-  })
-  return days;
+      <ul>
+        {
+        props.days.map(day => 
+          <DayListItem
+            key={day.id}
+            name={day.name}
+            spots={day.spots}
+            selected={day.name === props.day}
+            setDay={props.setDay}
+          />
+        )
+      }
+    </ul>
+  );
 }
---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "components/InterviewerListItem.scss";
+
+const classnames = require("classnames");
+
+export default function InterviewerListItem(props) {
+  const interviewersClass = classnames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+  })
+  return (
+    <li className={interviewersClass} onClick={() => props.setInterviewer(props.id)}>
+      <img
+        className="interviewers__item-image"
+        onClick={props.onClick}
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected? props.name : ""}
+    </li>    
+  );
+}
