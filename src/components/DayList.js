a4e49c3fd2fad77aa395766961016fcc52e3b732
Matthew Charles Chan <matthew.charles.chan@gmail.com>
2020-02-17 13:30:53 -0800

    uplift day create day hook

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import DayListItem from "components/DayListItem";
 
 export default function DayList(props) {
-  const DayListItems = props.days.map(day => {
+  const days = props.days.map(day => {
     return (
       <DayListItem
         key={day.id}
@@ -13,5 +13,5 @@ export default function DayList(props) {
       />
     );
   })
-  return DayListItems;
+  return days;
 }
