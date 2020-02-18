5fc3d53ebb6fa2bcaad3d59147f7ff5c133401bc
Matthew Charles Chan <matthew.charles.chan@gmail.com>
2020-02-17 16:01:33 -0800

    refactor setinterviiewer function

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -8,14 +8,14 @@ export default function InterviewerListItem(props) {
     "interviewers__item--selected": props.selected,
   })
   return (
-    <li className={interviewersClass} onClick={() => props.setInterviewer(props.id)}>
+    <li className={interviewersClass} onClick={props.setInterviewer}>
       <img
         className="interviewers__item-image"
         onClick={props.onClick}
         src={props.avatar}
         alt={props.name}
       />
-      {props.selected? props.name : ""}
+      {props.selected ? props.name : "" }
     </li>    
   );
 }
